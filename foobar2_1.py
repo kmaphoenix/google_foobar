028a3eaacec98b2479956cc1cf72a425a483c07a
kmaphoenix <kmaphoenix@gmail.com>
2020-09-22 00:38:59 -0500

    updating problem statements and doc strings

--- foobar2_1.py
+++ foobar2_1.py
@@ -1,3 +1,45 @@
+""" Google Foobar Level 2, Round 1 - Bunny Prisoner Locating
+
+Keeping track of Commander Lambda's many bunny prisoners is starting to get 
+tricky. You've been tasked with writing a program to match bunny prisoner IDs 
+to cell locations. The LAMBCHOP doomsday device takes up much of the interior 
+of Commander Lambda's space station, and as a result the prison blocks have an
+unusual layout. They are stacked in a triangular shape, and the bunny prisoners
+are given numerical IDs starting from the corner, as follows:
+
+| 7
+| 4 8
+| 2 5 9
+| 1 3 6 10
+
+Each cell can be represented as points (x, y), with x being the distance from 
+the vertical wall, and y being the height from the ground. For example, the 
+bunny prisoner at (1, 1) has ID 1, the bunny prisoner at (3, 2) has ID 9, and 
+the bunny prisoner at (2,3) has ID 8. This pattern of numbering continues 
+indefinitely (Commander Lambda has been taking a LOT of prisoners). 
+
+Write a function answer(x, y) which returns the prisoner ID of the bunny at 
+location (x, y). Each value of x and y will be at least 1 and no greater than 
+100,000. Since the prisoner ID can be very large, return your answer as a 
+string representation of the number.
+
+Test cases
+==========
+Inputs:
+    (int) x = 2
+    (int) y = 3
+Output:
+    (string) "8"
+
+Inputs:
+    (int) x = 5
+    (int) y = 10
+Output:
+    (string) "96"
+
+Time to solve: 72 hours.
+"""
+
 def solution(x,y):
     # num = 1
     start_x = 1
@@ -21,4 +63,5 @@ def solution(x,y):
     return str(value)
 
 if __name__ == '__main__':
-    solution(2,3)
+    # solution(2,3)
+    solution(5,10)
